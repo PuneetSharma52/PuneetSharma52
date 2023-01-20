41e8a636165fe5c0dbcb44d52d6ecd927377770a
Puneet Sharma <72139944+PuneetSharma52@users.noreply.github.com>
2023-01-20 16:32:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Personal Portfolio Website.😎
+
+Visit Here to See [Website](https://logan1x.github.io).
---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const projectData = [
+  {
+    projectName: "Codex",
+    projectDate: "December 2022",
+    projectDescription:
+      "Can generate anything using OpenAI",
+    tags: ["HTML5", "ExpressJS", "Vite", "VanillaJS"],
+    link: "https://codex-chat-gpt-clone.vercel.app/",
+    repo: "https://github.com/PuneetSharma52/Codex-chatGPT-clone",
+  },
+
+  {
+    projectName: "Calculator",
+    projectDate: "October 2022",
+    projectDescription:
+      "A simple Calculator app.",
+    tags: ["HTML5", "TailwindCSS", "ReactJS"],
+    link: "https://calculator-app-alpha-vert.vercel.app/",
+    repo: "https://github.com/PuneetSharma52/calculator-app",
+  },
+
+  {
+    projectName: "TrackMyPocket",
+    projectDate: "June 2020",
+    projectDescription:
+      "A minimal Android Expense Tracking app.",
+    tags: ["Android", "RoomDatabase"],
+    link: "#",
+    repo: "https://github.com/bholabalak/TrackMyPocket",
+  },
+
+  {
+    projectName: "Food Ordering System",
+    projectDate: "August 2019",
+    projectDescription:
+      "A Food Ordering System GUI Application.",
+    tags: ["C++"],
+    link: "#",
+    repo: "#",
+  },
+
+  {
+    projectName: "Railway Reservation System",
+    projectDate: "January 2019",
+    projectDescription:
+      "A Railway Reservation System GUI Application.",
+    tags: ["C++"],
+    link: "#",
+    repo: "#",
+  },
+
+  {
+    projectName: "TIC-TAC-TOE Game",
+    projectDate: "September 2018",
+    projectDescription:
+      "A simple tic-tac-toe console game.",
+    tags: ["C++"],
+    link: "#",
+    repo: "#",
+  }, 
+
+
+];
